baa8a74114fa08885791141b8359d3fd55015c7c
Malori <abo7fg@virginia.edu>
2025-04-21 14:16:03 -0400

    Updated start up script to launch ROS ASV launch package.

--- maddy_startup.bash
+++ maddy_startup.bash
@@ -2,14 +2,14 @@
 
 # Run from home/maddy on the ASV computer
 echo "Today is " `date`
+cd ~/ros2_ws
+source install/setup.bash
 
 gst-launch-1.0 udpsrc port=5600 ! queue ! udpsink host=172.16.0.16 port=5502 &
 
 mavproxy.py --master=/dev/ttyACM0 --out=udpbcast:172.16.0.255:14552 --out=udpbcast:localhost:14553 --daemon &
-mavproxy.py --master=udpin:192.168.2.1:14550 --out=udpbcast:172.16.0.255:14550 --out=udpbcast:localhost:14551 --daemon $
+mavproxy.py --master=udpin:192.168.2.1:14550 --out=udpbcast:172.16.0.255:14550 --out=udpbcast:localhost:14551 --daemon &
 
-cd ~/ros2_ws
-source install/setup.bash
 ros2 launch bluerov asv.launch.py && fg
 
 
