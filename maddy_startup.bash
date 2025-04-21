c934075d26a9d8c3582b1ed5ebedf907c1151a9a
Malori <abo7fg@virginia.edu>
2025-04-21 14:39:12 -0400

    Added logic to startup script to make it easy to change which network the vehicle is connected to (between VICTOR and NETGEAR21 on the portable router).

--- maddy_startup.bash
+++ maddy_startup.bash
@@ -5,10 +5,26 @@ echo "Today is " `date`
 cd ~/ros2_ws
 source install/setup.bash
 
-gst-launch-1.0 udpsrc port=5600 ! queue ! udpsink host=172.16.0.16 port=5502 &
+NETWORK=VICTOR
 
-mavproxy.py --master=/dev/ttyACM0 --out=udpbcast:172.16.0.255:14552 --out=udpbcast:localhost:14553 --daemon &
-mavproxy.py --master=udpin:192.168.2.1:14550 --out=udpbcast:172.16.0.255:14550 --out=udpbcast:localhost:14551 --daemon &
+case $NETWORK in
+
+  VICTOR)
+    GATE=172.16.0
+    GCS=172.16.0.16
+    ;;
+
+  PORTABLE)
+    GATE=192.168.1
+    GCS=192.168.1.35
+    ;;
+esac
+
+
+gst-launch-1.0 udpsrc port=5600 ! queue ! udpsink host=$GCS port=5502 &
+
+mavproxy.py --master=/dev/ttyACM0 --out=udpbcast:$GATE.255:14552 --out=udpbcast:localhost:14553 --daemon &
+mavproxy.py --master=udpin:192.168.2.1:14550 --out=udpbcast:$GATE.255:14550 --out=udpbcast:localhost:14551 --daemon &
 
 ros2 launch bluerov asv.launch.py && fg
 
