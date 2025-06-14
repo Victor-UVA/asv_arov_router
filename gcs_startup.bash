df954659af887c2d492d549948af505b3744a235
MaloriOxford <abo7fg@virginia.edu>
2025-06-14 12:15:47 -0400

    Added bash script to run on the GCS to handle the bulk of MAVLink and video routing, plus to launch the ROS launch file so that ROS is not running on the surface vehicle computer.
    Edited other launch files for the same.

---
+++ gcs_startup.bash
@@ -0,0 +1,32 @@
+#!/usr/bin/bash
+
+# Run from ~ on the GCS computer
+# bash ros2_ws/src/asv_arov_router/gcs_startup.bash
+echo "Today is " `date`
+cd ~/ros2_ws
+source install/setup.bash
+
+NETWORK=VICTOR
+
+case $NETWORK in
+
+  VICTOR)
+    GATE=172.16.0
+    ASV=$GATE.17 # TODO get the right address
+    ;;
+
+  PORTABLE)
+    GATE=192.168.1
+    ASV=$GATE.4
+    ;;
+esac
+
+gst-launch-1.0 udpsrc port=5500 ! queue ! multiudpsink clients=localhost:5501,localhost:5502 &
+
+mavproxy.py --master=udpin:$ASV:14552 --out=udpbcast:localhost:14552 --out=udpbcast:localhost:14553 --daemon & # ASV 
+mavproxy.py --master=udpin:$ASV:14550 --out=udpbcast:localhost:14550 --out=udpbcast:localhost:14551 --daemon & # AROV 
+
+ros2 launch asv_arov_router asv.launch.py && fg
+
+
+wait
--- maddy_startup.bash
+++ maddy_startup.bash
@@ -3,8 +3,8 @@
 # Run from home/maddy on the ASV computer
 # bash ros2_ws/src/asv_arov_router/maddy_startup.bash
 echo "Today is " `date`
-cd ~/ros2_ws
-source install/setup.bash
+# cd ~/ros2_ws
+# source install/setup.bash
 
 NETWORK=VICTOR
 
@@ -12,23 +12,23 @@ case $NETWORK in
 
   VICTOR)
     GATE=172.16.0
-    GCS=172.16.0.17
+    GCS=$GATE.17
     ;;
 
   PORTABLE)
     GATE=192.168.1
-    GCS=192.168.1.35
+    GCS=$GATE.35
     ;;
 esac
 
 
-gst-launch-1.0 udpsrc port=5600 ! queue ! udpsink host=$GCS port=5502 &
+gst-launch-1.0 udpsrc port=5600 ! queue ! udpsink host=$GCS port=5500 &
 
 # mavproxy.py --master=/dev/ttyACM1 --out=udpbcast:$GATE.255:14552 --out=udpbcast:localhost:14553 --daemon --cmd="set moddebug 2; module load optitrack; optitrack set server $SERVER; optitrack set client $CLIENT; optitrack set obj_id $OBJ_ID; optitrack start;" &
-mavproxy.py --master=/dev/ttyACM0 --out=udpbcast:$GATE.255:14552 --out=udpbcast:localhost:14553 --daemon &
-mavproxy.py --master=udpin:192.168.2.1:14550 --out=udpbcast:$GATE.255:14550 --out=udpbcast:localhost:14551 --daemon &
+mavproxy.py --master=/dev/ttyACM0 --out=udpbcast:$GATE.255:14552 --daemon & # ASV --out=udpbcast:$GATE.255:14553
+mavproxy.py --master=udpin:192.168.2.1:14550 --out=udpbcast:$GATE.255:14550 --daemon & # AROV --out=udpbcast:$GATE.255:14551
 
-ros2 launch asv_arov_router asv.launch.py && fg
+# ros2 launch asv_arov_router asv.launch.py && fg
 
 
 wait
