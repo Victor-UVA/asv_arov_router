56e1b4aa04c9e23a0d2ebfc969f730c6485aeac7
MaloriOxford <abo7fg@virginia.edu>
2025-06-20 11:29:54 -0400

    Added output for ASV MAVProxy to the OptiTrack base station.

--- gcs_startup.bash
+++ gcs_startup.bash
@@ -13,17 +13,19 @@ case $NETWORK in
   VICTOR)
     GATE=172.16.0
     ASV=$GATE.18
+    OPTI=$GATE.22
     ;;
 
   PORTABLE)
     GATE=192.168.1
     ASV=$GATE.4
+    OPTI=$GATE.22
     ;;
 esac
 
 gst-launch-1.0 udpsrc port=5500 ! queue ! multiudpsink clients=127.0.0.1:5501,127.0.0.1:5502 &
 
-mavproxy.py --master=udp:0.0.0.0:14553 --out=udpbcast:localhost:14554 --out=udpbcast:localhost:14555 --daemon & # ASV 
+mavproxy.py --master=udp:0.0.0.0:14553 --out=udpbcast:localhost:14554 --out=udpbcast:localhost:14555 --out=udpbcast:$OPTI:14556 --daemon & # ASV 
 mavproxy.py --master=udp:0.0.0.0:14550 --out=udpbcast:localhost:14551 --out=udpbcast:localhost:14552 --daemon & # AROV 
 
 ros2 launch asv_arov_router asv.launch.py && fg
